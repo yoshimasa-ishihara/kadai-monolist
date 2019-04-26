68ad0aa2a9de1920fb87137cb2887d1ce9ba592f
bassa <yosshi.spurs@gmail.com>
2019-04-26 23:58:07 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
     delete 'logout', to: 'sessions#destroy'
   
     get 'signup', to: 'users#new'
+    get 'rankings/want', to: 'rankings#want'
+    get 'rankings/have', to: 'rankings#have'
     resources :users, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   
     resources :items, only: [:show, :new]
     resources :ownerships, only: [:create, :destroy]
